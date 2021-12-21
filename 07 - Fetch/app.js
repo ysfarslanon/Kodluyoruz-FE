bd1ed7ed36fb6e48020c36a601d9227a72f92396
Yusuf ARSLAN <arslanon@outlook.com>
2021-12-21 20:25:10 +0300

    07 - Fetch

--- 07 - Fetch/app.js
+++ 07 - Fetch/app.js
@@ -2,13 +2,19 @@ document.querySelector("#button1").addEventListener("click", getTextFile);
 document.querySelector("#button2").addEventListener("click", getJSON);
 document.querySelector("#button3").addEventListener("click", getApi);
 
+let text1 = document.querySelector(".text-1");
+let text2 = document.querySelector(".text-2");
+let text3 = document.querySelector(".text-3");
+
 function getTextFile() {
   fetch("./helpers/textfile.txt")
     .then((res) => {
       return res.text();
     })
     .then((data) => {
-      document.getElementById("output").innerHTML += data;
+      let div = document.createElement("div");
+      div.innerHTML = data;
+      text1.appendChild(div);
     });
 }
 
@@ -20,22 +26,33 @@ function getJSON() {
     .then((data) => {
       let output = "<ul>";
       data.forEach((article) => {
-        output += `<li>Başlık: ${article.title} - İçerik: ${article.body}</li>`;
+        output += `<li>
+        <div class="card" style="width: 18rem;">
+        <img src="${article.img}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${article.text}</h5>
+          <p class="card-text">${article.body}</p>
+        </div>
+      </div>
+        </li>`;
       });
+      
       output += "</ul>";
-      document.getElementById("output").innerHTML += output;
+      text2.innerHTML = output;
     });
 }
 
 function getApi() {
-    fetch("https://api.github.com/users")
-    .then(res=>{ return res.json();})
-    .then(data => {
-        let output = "<ul>";
+  fetch("https://api.github.com/users")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      let output = "<ul>";
       data.forEach((user) => {
         output += `<li>Kullanıcı adı: ${user.login}</li>`;
       });
       output += "</ul>";
-      document.getElementById("output").innerHTML += output;
-    })
+      document.querySelector(".text-3").innerHTML += output;
+    });
 }
